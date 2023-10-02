898ea9023223ce9a688d59736468aab9a2ec8579
Kevin Patto <kevp2010@gmail.com>
2023-10-02 12:12:56 -0500

    added initial variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// base API URLs for OpenBreweryDB & OpenWeather
+var breweryApiUrl = "https://api.openbrewerydb.org/v1/breweries/";
+var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast";
+
+var weatherApiKey = ""; // add key here for testing
+
+var searchCity = ""; // city being searched, updated upon 
+
