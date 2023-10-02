a3218a370217234b3032fed8eacc7cbfb96d1e51
Kevin Patto <kevp2010@gmail.com>
2023-10-02 12:34:11 -0500

    added 3 functions & pseudocode for them

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
 var weatherApiKey = ""; // add key here for testing
 
-var searchCity = ""; // city being searched, updated upon 
+var searchCity = ""; // city being searched, updated upon searchByCity()
 
+function init() {
+    // this initial function will set up the page with a blank search result box and blank weather box
+}
+
+function renderWeather() {
+    // this function will render the weather on the right section
+}
+
+function searchByCity() {
+    // this function will take the user input and attempt to search by city in OpenBreweryDB
+    // if the city is invalid, it will inform the user in some way (TBD, just not using alert and stuff)
+    // if it is valid, a list of breweries will be shown and the weather section will be updated using renderWeather()
+    // set searchCity to the userinput for clarity
+}
