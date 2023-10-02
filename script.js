7f72ddc5aa3f6c3ccfe8dbb72e7d6bec5519f42a
Kevin Patto <kevp2010@gmail.com>
2023-10-02 12:48:25 -0500

    swapped order of functions and added renderResults

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var weatherApiKey = ""; // add key here for testing
 var searchCity = ""; // city being searched, updated upon searchByCity()
 
 // element variables
-// need variables for: search box div, input & button; weather box div
+// need variables for: search box div/section, input & button, and search results div; weather box div
 
 
 // FUNCTIONS
@@ -16,15 +16,20 @@ function init() {
     // this initial function will set up the page with a blank search result box and blank weather box
 }
 
-function renderWeather() {
-    // this function will render the weather on the right section
-}
-
 function searchByCity() {
     // this function will take the user input and attempt to search by city in OpenBreweryDB
-    // if the city is invalid, it will inform the user in some way (TBD, just not using alert and stuff)
-    // if it is valid, a list of breweries will be shown and the weather section will be updated using renderWeather()
+    // if the city is invalid or has no results, it will inform the user in some way (TBD, just not using alert and stuff)
+    // if it is valid, a list of breweries will be shown using renderResults() and the weather section will be updated using renderWeather()
     // set searchCity to the userinput for clarity
+}
+
+function renderWeather() {
+    // this function will render the weather on the right section
+    // it will be called if searchByCity() recieves a valid city from the user's input
+}
+
+function renderResults() {
+    // this function will render the search results in the left section search results div
 }
 
 // eventlisteners
