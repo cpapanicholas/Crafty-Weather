edf5cba1db1d5af4c8541c5fe358eb4529310d06
Kevin Patto <kevp2010@gmail.com>
2023-10-03 10:54:10 -0500

    changed fetch request for weather to be based on user input

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ var searchCity = ""; // city being searched, updated upon searchByCity()
 // weather box section (all children will be generated in renderWeather())
 var searchInputEl = document.querySelector("#search-input");
 var searchButtonEl = document.querySelector("#search-button");
-var testEl = document.querySelector("#test");
 
 
 // FUNCTIONS
@@ -26,27 +25,25 @@ function searchByCity(event) {
     // if the city is invalid or has no results, it will inform the user in some way (TBD, just not using alert and stuff)
     // if it is valid, a list of breweries will be shown using renderResults() and the weather section will be updated using renderWeather()
     // set searchCity to the userinput for clarity
-    event.preventDefault();
+    event.preventDefault(); // prevents CORS errors, somehow
 
     searchCity = searchInputEl.value.split(' ').join('_').toLowerCase(); // replace spaces with underscores for api url
     console.log(searchCity);
-    // fetch from OpenWeather
 
-    // fetch from OpenBreweryDB
-    // fetch (weatherApiUrl)
-    fetch ("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=" + weatherApiKey)
-        .then (function (response) {
+    // fetch from OpenWeather
+    fetch (weatherApiUrl + "?q=" + searchCity + "&appid=" + weatherApiKey)
+        .then(function (response) {
             return response.json();
         })
-        .then (function (data) {
+        .then(function (data) {
             console.log(data);
         });
-    // fetch (breweryApiUrl + "?by_city=" + searchCity + "&per_page=3")
-    fetch ("https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=3")
-        .then (function (response) {
+    // fetch from OpenBreweryDB
+    fetch(breweryApiUrl + "?by_city=" + searchCity + "&per_page=3")
+        .then(function (response) {
             return response.json();
         })
-        .then (function (data) {
+        .then(function (data) {
             console.log(data);
         });
 }
