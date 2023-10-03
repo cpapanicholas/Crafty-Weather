b72209f04eb0cc1435dd179413394e94689d4af9
Kevin Patto <kevp2010@gmail.com>
2023-10-03 10:49:53 -0500

    preventDefault made it work. added request to OpenWeather as well

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var searchCity = ""; // city being searched, updated upon searchByCity()
 // weather box section (all children will be generated in renderWeather())
 var searchInputEl = document.querySelector("#search-input");
 var searchButtonEl = document.querySelector("#search-button");
+var testEl = document.querySelector("#test");
 
 
 // FUNCTIONS
@@ -20,13 +21,26 @@ function init() {
     // this initial function will set up the page with a blank search result box and blank weather box
 }
 
-function searchByCity() {
+function searchByCity(event) {
     // this function will take the user input and attempt to search by city in OpenBreweryDB
     // if the city is invalid or has no results, it will inform the user in some way (TBD, just not using alert and stuff)
     // if it is valid, a list of breweries will be shown using renderResults() and the weather section will be updated using renderWeather()
     // set searchCity to the userinput for clarity
-    searchCity = searchInputEl.textContent.split(' ').join('_').toLowerCase(); // replace spaces with underscores for api url
+    event.preventDefault();
+
+    searchCity = searchInputEl.value.split(' ').join('_').toLowerCase(); // replace spaces with underscores for api url
     console.log(searchCity);
+    // fetch from OpenWeather
+
+    // fetch from OpenBreweryDB
+    // fetch (weatherApiUrl)
+    fetch ("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=" + weatherApiKey)
+        .then (function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            console.log(data);
+        })
     // fetch (breweryApiUrl + "?by_city=" + searchCity + "&per_page=3")
     fetch ("https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=3")
         .then (function (response) {
